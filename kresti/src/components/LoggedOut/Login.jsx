7a6ace746320a48a1586d7c1d3e8530fc3e8806d
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-06-01 23:22:56 +0100

    Kresti Insurance

--- kresti/src/components/LoggedOut/Login.jsx
+++ kresti/src/components/LoggedOut/Login.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Login.css';
-import NavBar from './Onboarding/NavBar'
-import BusinessGoals from '../../assets/Business_goals_1.jpg'
+import NavBar from './Onboarding/NavBar';
+import BusinessGoals from '../../assets/Business_goals_1.jpg';
+import FootBar from './Onboarding/FootBar';
 
 function Login() {
   return (
@@ -14,12 +15,13 @@ function Login() {
         <div className="logn-col-2">
             <h1 className="lgn-h1">Let's get started</h1>
             <div className="lgn-input">
-                <input type="email" name="" id="" placeholder="Enter your username"/>
-                <input type="password" name="" id="" placeholder="Password"/>
-                <input type="submit" value="Log in" />
+                <input type="email" name="" id="" placeholder="Enter your username" className='lgn-uName'/>
+                <input type="password" name="" id="" placeholder="Password" className='lgn-pwd'/>
+                <input type="submit" value="Log in" className='lgn-sbmt'/>
             </div>
         </div>
       </section>
+      <FootBar />
     </div>
   )
 }
